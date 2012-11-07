94d30a7759af8931b46ecdd25c3d1e0891efbfcb
Anton Ovchinnikov <revolver112@gmail.com>
2012-11-07 21:49:46 +0400

    Code clean-up

--- alposter.pl
+++ alposter.pl
@@ -1,32 +1,35 @@
-#!/usr/bin/perl -w
+#!/usr/bin/env perl
 # # #
 # Alposter: Alpaca flag poster, v1.1
 # # #
 
 use strict;
+use warnings;
 use IO::Socket::INET;
 
 ### config
-my $remhost = '10.11.0.1';
-my $port = 1;
+my $remhost = '192.168.1.50';
+my $port = 1234;
 my $proto = 'tcp';
 
 ### flags
-my $flag_regex = '\w{40}';
+my $flag_regex = '\w{6}';
 
 ### timeouts
 my $wait_after_each_flag = 0;
 my $timeout = 1; # for waiting after each flag
 my $final_timeout = 2;
 
-### 
+###
 
 $| = 1;
+
+print "> Connecting to: $remhost:$port...\n";
 my $sock = IO::Socket::INET->new(	PeerAddr	=> $remhost,
-									PeerPort	=> $port,
-									Proto		=> $proto,
-									)
-or die "Can't open socket: $!";
+                                  PeerPort	=> $port,
+                                  Proto		=> $proto,
+                                )
+  or die "Can't open socket: $!";
 
 
 my @flags;
@@ -35,106 +38,90 @@ my $filemode = 0;
 
 my $peeraddr = $sock->peerhost();
 my $peerport = $sock->peerport();
-print "Connected to: $peeraddr:$peerport\nDon't be afraid and post some flags.\n";
+print "> Connected to: $peeraddr:$peerport\n> Don't be afraid and post some flags.\n";
 
 # arguments are present (filenames)
-if (@ARGV){
-	$filemode = 1;
-# generate flag list (from files)
-	while (<>){
-		my @tempfl = /$flag_regex/g;
-		push @flags, @tempfl;
-	}
+if (@ARGV) {
+  $filemode = 1;
+  # generate flag list (from files)
+  while (<>) {
+    my @tempfl = /$flag_regex/g;
+    push @flags, @tempfl;
+  }
 
-	$flag_array_size = @flags;
-	print "Flags detected: $flag_array_size\n";
-	if ($flag_array_size == 0) {
-		exit(0);
-	}
-
-#	print "$_\n" for @flags;
-
+  $flag_array_size = @flags;
+  print "> Flags detected: $flag_array_size\n";
+  if ($flag_array_size == 0) {
+    exit(0);
+  }
 }
-
-
-
 # read from standard input
-else{ 
-	print "Reading flags from standard input...\n";
+else {
+  print "> Reading flags from standard input...\n";
 }
+
 
 my $pid = fork();
 
-if ($pid < 0){
-	die "Cannot fork! $!\n";
+if ($pid < 0) {
+  die "> Cannot fork! $!\n";
 }
 
-# child
-if ($pid == 0){
+# child: read from socket
+if ($pid == 0) {
 
-	my ($curdata, $data);
+  my ($curdata, $data);
 
-#	while ($count = sysread($sock, $data, 1024)){
-#		chomp;
-#		if (!$filemode){
-#			print "$data\n";
-#			next;
-#		}
-#		my $newflag = shift @flags;
-#		print "$newflag: $_\n";
-#	}
+  while (sysread($sock, $curdata, 1024)) {
+    $data .= $curdata;
+    print $curdata;
+  }
 
+  print "CONNECTION LOST!!!\n";
+  kill SIGTERM => getppid();
+}
 
-	while (sysread($sock, $curdata, 1024)){
-		$data .= $curdata;
-		print $curdata;
-	}
+# parent: write to socket
+elsif ($pid > 0) {
 
-	print "CONNECTION LOST!!!\n";
-	kill SIGTERM => getppid();
+  # read from files
+  if ($filemode) {
+    sleep 1;
+    for (@flags) {
+      print $sock "$_\n";
+      if ($wait_after_each_flag) {
+        sleep $timeout;
+      }
+    }
+  }
 
-} 
+  # read from standard input
+  else {
+    while (<STDIN>) {
+      if (!$sock->connected) {
+        print "CONNECTION LOST\n";
+        goto FINISH;
+      }
+      if (/^q(uit)?$|^e(xit)?$/i) {
+        print ">> Quitting... <<<\n";
+        goto FINISH;
+      }
 
-# parent
-elsif ($pid > 0){
+      # send flags if any
+      if (/($flag_regex)/) {
+        push @flags, $1;
+        print $sock "$1\n";
+      }
+      else {
+        print "> Ooops.. Are you sure it looks like a flag?\n"
+      }
 
-	if ($filemode){
-		sleep 1;
-		for (@flags){
-			print $sock "$_\n";
-			if ($wait_after_each_flag){
-				sleep $timeout;
-			}
-		}	
-	} 
-	
-	# read from standard input
-	else{
+    }
+  }
 
-		while (<STDIN>){
-			if (!$sock->connected){
-				print "CONNECTION LOST\n";
-				goto FINISH;
-			}
-			if (/^q(uit)?$|^e(xit)?$/i){
-				print ">> Quitting... <<<\n";
-				goto FINISH;
-			}
-	
-			if (/($flag_regex)/){
-				push @flags, $1;
-				print $sock "$1\n";
-			}
-			else {
-				print "Ooops.. Are you sure it looks like a flag?\n"
-			}
-
-		}
-	}
-
-	sleep $final_timeout;
-FINISH:	
-	close $sock;
-	kill SIGTERM => $pid;
-} 
+  sleep $final_timeout;
+  FINISH:
+  close $sock;
+  kill SIGTERM => $pid;
+}
 
